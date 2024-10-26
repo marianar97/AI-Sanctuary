e2c97522684017a9644631d697c5b39b00b1c6f9
Mariana <marianar@andrew.cmu.edu>
2024-10-26 14:58:14 -0400

    adding tagging filter functionality

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,20 +2,39 @@ import {useEffect, useState} from "react";
 import '../CSS/header.css';
 import HeroSection from "./HeroSection/HeroSection";
 import VideoGrid from "./Videos/VideoGrid";
+import Tags from "./Tags/Tags";
 
 export default function HomePage() {
     const [videos, setVideos] = useState([]);
     const [activeVideo, setActiveVideo] = useState(null);
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedTags, setSelectedTags] = useState([]);
 
     const openVideo = (videoId) => setActiveVideo(videoId)
     const closeVideo = () => setActiveVideo(null)
     const handleSearch = (event) => {
         setSearchTerm(event.target.value);
     }
+    const handleTagClick = (tag) => {
+        console.log("click", tag);
+        if (selectedTags.includes(tag)) {
+            setSelectedTags(selectedTags.filter(t => t !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    }
 
-    const filteredVideos = videos.filter(video => video.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    let filteredVideos = videos.filter(video => video.title.toLowerCase().includes(searchTerm.toLowerCase()))
     
+    const filteredVideosByTags = (videos) => {
+        if (selectedTags.length === 0) {
+            return videos;
+        }
+        return videos.filter(video => selectedTags.every(tag => video.tags.includes(tag)));
+    }
+
+    filteredVideos = filteredVideosByTags(filteredVideos);
+
     useEffect(() => {
         fetch("videos.json")
         .then((response)=>response.json())
@@ -25,8 +44,9 @@ export default function HomePage() {
 
     return (
         <div className='flex flex-col w-full items-center'>
-           <HeroSection value={searchTerm} handleSearch={handleSearch} ></HeroSection>
-           <VideoGrid videos={filteredVideos} openVideo={openVideo} activeVideo={activeVideo} closeVideo={closeVideo}></VideoGrid>
+            <HeroSection value={searchTerm} handleSearch={handleSearch} ></HeroSection>
+            <Tags onClick={handleTagClick}></Tags>
+            <VideoGrid videos={filteredVideos} openVideo={openVideo} activeVideo={activeVideo} closeVideo={closeVideo}></VideoGrid>
         </div>
     );
 }
---
+++ src/components/Tags/Tags.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Tag from "./Tag";
+
+export default function Tags({ onClick }) {
+    const tags = ["Intermediate", "Foundation", "Beginner", "Advanced", "Applied", "Theory"];
+    return (
+        <div className="mb-8 flex flex-wrap gap-2">
+            {
+                tags.map(tag => <Tag key={tag} tag={tag} clickFunc={onClick}></Tag>)
+            }
+        </div>
+    );
+}
